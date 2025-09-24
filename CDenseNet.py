691206b7db690e20323c08f788a2f8d97e8b9581
Noah <science103555@gmail.com>
2025-09-24 15:55:24 +0800

    Initial commit

---
+++ CDenseNet.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class LDB(nn.Module):
+  def __init__(self, in_channel: int, t: float = 0.5):
+    pass
+
+  def forward(self, x):
+    pass
+
+class CDenseNet(nn.Module):
+  def __init__(self, n: int = 16, t: float = 0.5):
+    pass
+
+  def forward(self, x):
+    pass
